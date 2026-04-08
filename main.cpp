5c56c4b7de12182dcd645252c3786605a5d7b921
defint-wq <defint.exe@gmail.com>
2026-04-08 17:04:14 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include "oop.h"
 
 int main () {
-    cout << "Kill you !!!" << endl;
+    Circle c;
+    Square s;
+    Triangle t;
+    int which;
+
+    while (true) {
+        cout << "1. Circle, 2. Square, 3. Triangle, 4. Exit\n" << endl;
+        cin >> which;
+        if (which == 4) break;
+
+        switch (which)
+        {
+        case 1: // Circle
+            break;
+        
+        case 2: // Square
+            break;
+
+        case 3: // Triangle
+            break;
+
+        default:
+            break;
+        }
+    }
     return 0;
 } 
