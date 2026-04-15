96b5e98111bf7e885acfe40c4b2aaed1add9c7fb
defint-wq <defint.exe@gmail.com>
2026-04-15 20:13:20 +0800

    lab7

--- lab7/cpp/max.cpp
+++ lab7/cpp/max.cpp
@@ -3,9 +3,7 @@
 int max(double *areas, int n) {
     int maxidx = 0;
     for (int i = 1; i < n; i++ ) {
-        if (*(areas + i) > *(areas + maxidx)) {
-            maxidx = i;
-        }
+        if (*(areas + i) > *(areas + maxidx)) maxidx = i;
     }
 
     return maxidx;
