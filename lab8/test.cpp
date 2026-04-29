eab0af0bddc668f9d9c0312f2f89094518310480
defint-wq <defint.exe@gmail.com>
2026-04-30 07:40:17 +0800

    lab8

---
+++ lab8/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    cout << "Ariunbolor: ";
+    char k[10];
+    cin >> k;
+
+    if (strcmp(k, "Huurhun") == 0) {
+        cout << "Oonhoo" << endl;
+    } else cout << "Solior" << endl;
+
+    return 0;
+}
