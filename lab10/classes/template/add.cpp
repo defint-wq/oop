26c377982eb02f4d6bdb8a218e3038816b88ba89
tssumiyabazar1199 <ts.sumiyabazar1199@gmail.com>
2026-05-12 18:12:24 +0800

    s

--- lab10/classes/template/add.cpp
+++ lab10/classes/template/add.cpp
@@ -1,1 +1,20 @@
 #include "template.h"
+#pragma once   // Давхар include-аас хамгаална
+
+template <typename T>
+void LinkedList<T>::add(T t) {
+    Node* node = new Node(t);
+
+    if (!head) {
+        // Жагсаалт хоосон — шинэ node нь head болно
+        head = node;
+    } else {
+        // Сүүл хүртэл явж, тэнд залгана
+        Node* cur = head;
+        while (cur->next)
+            cur = cur->next;
+        cur->next = node;
+    }
+
+    size++;
+}
