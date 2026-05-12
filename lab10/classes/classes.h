ab9bf9274c370b4a66928c646feb6b71cd568876
defint-wq <defint.exe@gmail.com>
2026-05-12 17:41:18 +0800

    tamir

--- lab10/classes/classes.h
+++ lab10/classes/classes.h
@@ -1,11 +1,5 @@
 #ifndef CLASSES_H
 #define CLASSES_H
 
-#include "child/child.h"
-#include "division/division.h"
-#include "employee/employee.h"
-#include "jobDescription/jobDescription.h"
-#include "person/person.h"
-#include "spouse/spouse.h"
 
 #endif
--- lab10/classes/include.h
+++ lab10/classes/include.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 
 #endif
