c640360268b4991f1b5a0996239e56e3450971a5
tssumiyabazar1199 <ts.sumiyabazar1199@gmail.com>
2026-05-12 18:35:38 +0800

    ss

--- lab10/classes/template/delete.cpp
+++ lab10/classes/template/delete.cpp
@@ -1,1 +1,26 @@
 #include "template.h"
+
+template <typename T>
+void LinkedList<T>::remove(int index) {
+    if (index < 0 || index >= _length || head == nullptr) {
+        return;
+    }
+
+    Node<T>* temp = head;
+
+    // 2. Хэрэв хамгийн эхний элементийг (index 0) устгах бол
+    if (index == 0) {
+        head = head->next;
+        delete temp;  
+    } 
+    else {
+        Node<T>* prev = nullptr;
+        for (int i = 0; i < index; i++) {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = temp->next;
+        delete temp; 
+    }
+    _length--;
+}
