86de6eb808849e84d81039c2b875ea1b4e480a02
Bilguun Ganbaatar <bilguunganbaatar@Bilguuns-MacBook-Pro-4.local>
2026-05-13 15:40:53 +0800

    lab10

--- lab10/classes/template/get.cpp
+++ lab10/classes/template/get.cpp
@@ -1,1 +1,17 @@
 #include "template.h"
+
+template <typename T>
+T List<T>::get(int index) {
+
+    if (index < 0 || index >= len) {
+        throw out_of_range("Index out of range");
+    }
+
+    Node<T>* temp = head;
+
+    for (int i = 0; i < index; i++) {
+        temp = temp->next;
+    }
+
+    return temp->shapes;
+}
--- lab10/classes/template/insert.cpp
+++ lab10/classes/template/insert.cpp
@@ -1,1 +1,28 @@
 #include "template.h"
+
+template <typename T>
+void List<T>::insert(T t, int index) {
+    if (index < 0 || index > len)
+        return;
+
+    Node<T>* newNode = new Node<T>();
+    newNode->shapes = t;
+    newNode->next = nullptr;
+
+    if (index == 0) {
+        newNode->next = head;
+        head = newNode;
+    } 
+    else {
+        Node<T>* temp = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp->next;
+        }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    len++;
+}
